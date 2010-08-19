6a155c512a26394014756c8f26dbb770f2f5f71a
RxSchuster <RxSchuster@52a7daa1-0b76-2490-7fad-64cc6dd1c89b>
2010-08-19 13:22:36 +0000

    added MPI_Barrier function

--- trunk/mpi4ncl/nbproject/Makefile-variables.mk
+++ trunk/mpi4ncl/nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=MPI-Linux-x86
+CND_PLATFORM_Debug=MPI-MacOSX
 CND_ARTIFACT_DIR_Debug=dist
 CND_ARTIFACT_NAME_Debug=libmpi4ncl.so
 CND_ARTIFACT_PATH_Debug=dist/libmpi4ncl.so
-CND_PACKAGE_DIR_Debug=dist/Debug/MPI-Linux-x86/package
-CND_PACKAGE_NAME_Debug=libmpi4ncl.so.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/MPI-Linux-x86/package/libmpi4ncl.so.tar
+CND_PACKAGE_DIR_Debug=dist/Debug/MPI-MacOSX/package
+CND_PACKAGE_NAME_Debug=libmpi4ncl.dylib.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MPI-MacOSX/package/libmpi4ncl.dylib.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux-x86
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
